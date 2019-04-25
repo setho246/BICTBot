627d1e4a4904a53051bd4bbea81f607edeb20352
unknown <setho246@gmail.com>
2019-04-25 20:57:54 +1000

    more crossing

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
     console.log("Recieved message from " + message.member.tag + " and it said" + message.content)
-    if (message.content.indexOf(ClientUser.tag) === 0) {
+    if (message.content.indexOf(client.user.tag) === 0) {
         var str = message.content.slice(13)
         message.channel.send(message.author.tag+" said *"+str+"* to me. **THE NERVE!**")
 
